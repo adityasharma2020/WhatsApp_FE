4db48533145db90c56995b71c47271460fe1a689
adityasharma2020 <aditya15116617@gmail.com>
2024-04-05 14:21:31 +0530

    refactor tailwind custom classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'class',
+  darkMode: 'class', // we just have to add class dark if we wnat to apply dark mode classes to work
   theme: {
     extend: {
       colors: {
